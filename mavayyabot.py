447fa7d59947e968892d23b54c9e48b7e36e5fac
poirot <94121379+poirot22@users.noreply.github.com>
2021-11-13 02:06:31 +0530

    Update mavayyabot.py

--- mavayyabot.py
+++ mavayyabot.py
@@ -3,10 +3,13 @@ import random
 import os
 from keep_online import keep_alive
 
+
 intents = discord.Intents.default()
 intents.members = True
+intents.emojis=True
 
 client=discord.Client(intents=intents)
+
 
 
 @client.event
@@ -15,6 +18,7 @@ async def on_message(message):
   username=str(message.author).split("#")[0]
   mess=message.content
 
+  #general mavayya phrases
   mavayya_maatalu=[
   f"entira {username} chusi kuda chudanattu velthunav...bagunava?",
   "manushulu manchollu ra, manashi antene manchodu!",
@@ -34,6 +38,8 @@ async def on_message(message):
     if i in mess:
 	    await message.channel.send(random.choice(mavayya_maatalu))
 
+
+  #list of cuss words
   bad_words=[
     "lauda",
     "modda",
@@ -45,24 +51,33 @@ async def on_message(message):
     "lawda",
     "lowda",
     "puku",
-    " fuck",
-    " ass " 
+    "fuck",
+    " ass ",
+    "ass\n"
   ]
 
   for i in bad_words:
     if i in mess:
       await message.channel.send(f"yenti ra {username}, yemitaa maatalu?")
   
+  #displays the avatar
+  if mess == "-avatar":
+    await message.channel.send(message.author.avatar_url)
+
+
+  #so that bot wouldn't summon itself
   if message.author==client.user:
     return
-  
 
+
+#welcome message when a member joins
 @client.event
 async def on_member_join(member):
   channel = client.get_channel(890388908813213727)
   await channel.send(f"Yem maa {member.mention} bavunnava..? Ye pusthakam chaduvtunnav e madya..?")
- 
 
+ 
+#keeping the bot online
 my_secret = os.environ['TOKEN']
 keep_alive()
 client.run(my_secret)
