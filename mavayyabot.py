375407b412e55d788afdf961bf403cce4b5e11f0
poirot <94121379+poirot22@users.noreply.github.com>
2021-11-18 23:24:16 +0530

    Update mavayyabot.py

--- mavayyabot.py
+++ mavayyabot.py
@@ -2,6 +2,8 @@ import discord
 import random
 import os
 from keep_online import keep_alive
+import time
+
 
 intents = discord.Intents.default()
 intents.members = True
@@ -9,18 +11,8 @@ intents.emojis=True
 
 client=discord.Client(intents=intents)
 
-hg2g={
-  "killjoy":0,
-  "seegana":0,
-  "ayaan":0,
-  "debugger":0
-}
-
 @client.event
 async def on_message(message):
-
- 
-
   username=str(message.author).split("#")[0]
   mess=message.content
 
@@ -42,8 +34,9 @@ async def on_message(message):
   f"అరేయి {username}, దాటగలిగే కష్టాల్నీనీ, ఆ కష్టాల్ని దాటే ధైర్యాన్నీ ఇవ్వమని దేవుణ్ణి అడగరా!"
 
   ]
-
+  
   mavayya=["mavayya","Mavayya"]
+  
   for i in mavayya:
     if i in mess:
 	    await message.channel.send(random.choice(mavayya_maatalu))
@@ -62,59 +55,44 @@ async def on_message(message):
     "lowda",
     "puku",
     " ass ",
-    "ass\n"
+    "ass\n",
+    " ass"
   ]
 
   for i in bad_words:
     if i in mess:
       await message.channel.send(f"yenti ra {username}, yemitaa maatalu?")
-
-  if mess=="-react":
-    emoji="wholesomemavayya"
-    mess.add_reaction(emoji)
   
   #displays the avatar
   if mess == "-avatar":
     await message.channel.send(message.author.avatar_url)
 
-  if mess == "-update hg2g":
-
-    await message.channel.send("ye ra, yenno chapter varaku?")
-
-    async def on_message(message):
-
-      if str(message.author).split("#")[0]=="KILLJOY":
-        hg2g["killjoy"]=int(message.content)
-      if str(message.author).split("#")[0]=="psychedelicpoops":
-        hg2g["seegana"]=int(message.content)
-      if str(message.author).split("#")[0]=="Ayaan":
-        hg2g["ayaan"]=int(message.content)
-      if str(message.author).split("#")[0]=="debugger":
-        hg2g["debugger"]=int(message.content)
-    
-  if mess == f"-progress {username}":
-      if username=="KILLJOY":
-        await message.channel.send(hg2g["killjoy"])
-      if username=="psychedelicpoops":
-        await message.channel.send(hg2g["seegana"])
-      if username=="Ayaan":
-        await message.channel.send(hg2g["ayaan"])
-      if username=="debugger":
-        await message.channel.send(hg2g["debugger"])
 
   #so that bot wouldn't summon itself
   if message.author==client.user:
     return
 
 
+
+
 #welcome message when a member joins
 @client.event
 async def on_member_join(member):
+  time.sleep(1)
   channel = client.get_channel(890388908813213727)
   await channel.send(f"Yem maa {member.mention} bavunnava..? Ye pusthakam chaduvtunnav e madya..?")
 
- 
+'''@client.event
+async def on_message_delete(message):
+  time.sleep(5)
+  botspam=client.get_channel(876497595441229824)
+  await botspam.send("ye ra, nee thappulanni ila lokaniki thelikunda daachestunnava?")'''
+
+
+
 #keeping the bot online
 my_secret = os.environ['TOKEN']
 keep_alive()
 client.run(my_secret)
+
+
